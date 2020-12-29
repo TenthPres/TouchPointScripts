b72ba1ed9a83d5e21ae48a46b2e4597f7b056248
James K <jkurtz@tenth.org>
2020-12-29 17:04:27 -0500

    Updating EmailAddressArchiving.  Probably done.

--- EmailAddressArchiving/EmailAddressArchiving.py
+++ EmailAddressArchiving/EmailAddressArchiving.py
@@ -1,21 +1,21 @@
 # This script does not require editing, but knock yourself out if you like.
 
 import json
-from pprint import pprint
 
 # Set with the Extra value name to use for the email address array.  Once this script runs once, you probably don't want to change it.
 evName = "EmailAddresses"
 
 query = '''(
-        EmailAddress2 <> ''
-        OR EmailAddress <> ''
+        EmailAddress2 <> '*@*'
+        OR EmailAddress <> '*@*'
         OR HasPeopleExtraField = '$evName_mv'
-    '''.replace("$evName", evName)
+    )'''.replace("$evName", evName)
     
 count = q.QueryCount(query)
 
-for p in q.QueryList(query, "PeopleId"): # TODO add count option when PR 1205 is released. 
-    print "<h2>" + p.Name + "</h2>"
+print "Saving Email Addresses to Extra Values..."
+
+for p in q.QueryList(query, "PeopleId", count+2): 
     
     addrs = []
     dirty = False
@@ -25,18 +25,21 @@ for p in q.QueryList(query, "PeopleId"): # TODO add count option when PR 1205 is
         addrs = json.loads(existingEv)
         
     movedEv = model.ExtraValueText(p.PeopleId, evName + "_mv");
-    if existingEv != '':
-        for a in json.loads(existingEv):
+    if movedEv != '':
+        for a in json.loads(movedEv):
             if a.lower() not in addrs:
                 addrs.append(a.lower())
+                dirty = True
         model.DeleteExtraValue(p.PeopleId, evName + "_mv")
                 
     if p.EmailAddress != None and p.EmailAddress.lower() not in addrs:
         addrs.append(p.EmailAddress.lower())
+        dirty = True
         
     if p.EmailAddress2 != None and p.EmailAddress2.lower() not in addrs:
         addrs.append(p.EmailAddress2.lower())
-        
-    model.AddExtraValueText(p.PeopleId, evName, json.dumps(addrs))
-    
-    pprint(json.dumps(addrs))
+        dirty = True
+
+    if dirty:
+        model.AddExtraValueText(p.PeopleId, evName, json.dumps(addrs))
+        print " updated " + p.Name + "\n"
