216bc325a052aeeb98375d861bdf0f12c62c985c
James K <jkrrv@jkrrv.org>
2021-01-08 07:40:38 -0500

    Expanding roles to include officer roles

--- RoleChecker/RoleChecker.py
+++ RoleChecker/RoleChecker.py
@@ -1,5 +1,8 @@
 print "Updating Access Roles...\n"
 
+
+####### ACCESS #######
+
 print " Remove Access role...\n"
 # Removes the Access role (and indrectly, all other special roles from anyone who isn't: a leader of an org OR a member of Staff or Session or Diaconate or Women's Cmte
 for p in q.QueryList('''
@@ -38,7 +41,9 @@ for p in q.QueryList('''
     '''):
     print "  " + p.Name + "\n"
     model.AddRole(p.PeopleId, "Access")
-
+    
+    
+####### ORGLEADERONLY #######
 
 print "\n Remove OrgLeadersOnly role\n"
 # Remove the OrgLeadersOnly role from Officers and Staff (who have Access role already from above)
@@ -69,8 +74,20 @@ for p in q.QueryList('''
     '''):
     print "  " + p.Name + "\n"
     model.AddRole(p.PeopleId, "OrgLeadersOnly")
-
-
+    
+    
+####### ATTENDANCE #######
+    
+print "\n Add Attendance role\n"
+# Add the attendance role for those who are leaders of orgs.  (Note that this doesn't discriminate the type of org.) 
+for p in q.QueryList('''
+    UserRole <> 3[Attendance]
+    AND MemberTypeCodes(  ) IN ( 159[Coordinator], 103[Director], 150[Host], 140[Leader], 1061[Principal], 160[Teacher] )
+    AND UserRole NOT IN ( 9[Developer], 18[Checkin], 46[APIOnly], 47[APIWrite] )
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Attendance")
+    
 print "\n Remove Attendance role\n"
 # Remove the attendance role from someone who is not the leader of an org. 
 for p in q.QueryList('''
@@ -83,12 +100,141 @@ for p in q.QueryList('''
     model.RemoveRole(p.PeopleId, "Attendance")
 
 
-print "\n Add Attendance role\n"
-# Add the attendance role for those who are leaders of orgs.  (Note that this doesn't discriminate the type of org.) 
+####### OFFICER ####### 
+    
+print "\n Remove Officer role\n"
+# Remove the officer role for those appropriate. 
 for p in q.QueryList('''
-    UserRole <> 3[Attendance]
-    AND MemberTypeCodes(  ) IN ( 159[Coordinator], 103[Director], 150[Host], 140[Leader], 1061[Principal], 160[Teacher] )
-    AND UserRole NOT IN ( 9[Developer], 18[Checkin], 46[APIOnly], 47[APIWrite] )
+    (
+        (
+        	IsMemberOf( Org=124[Session] ) <> 1[True]
+        	AND IsMemberOf( Org=125[Diaconate] ) <> 1[True]
+        )
+        OR GenderId <> 1[Male]
+    )
+    AND UserRole = 51[Officer]
     '''):
     print "  " + p.Name + "\n"
-    model.AddRole(p.PeopleId, "Attendance")
+    model.RemoveRole(p.PeopleId, "Officer")
+    
+print "\n Add Officer role\n"
+# Add the officer role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) = 1[True]
+    	OR IsMemberOf( Org=125[Diaconate] ) = 1[True]
+    )
+    AND UserRole <> 51[Officer]
+    AND GenderId = 1[Male]
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Officer")
+    
+    
+####### SESSION #######
+    
+print "\n Remove Session role\n"
+# Remove the session role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) <> 1[True]
+    )
+    AND UserRole = 52[Session]
+    '''):
+    print "  " + p.Name + "\n"
+    model.RemoveRole(p.PeopleId, "Session")
+    
+print "\n Add Session role\n"
+# Add the session role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) = 1[True]
+    )
+    AND UserRole <> 52[Session]
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Session")
+    
+
+####### SESSION+DIACONATE #######
+    
+print "\n Remove Session+Diaconate role\n"
+# Remove the Session+Diaconate role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) <> 1[True]
+    	AND IsMemberOf( Org=125[Diaconate] ) <> 1[True]
+    )
+    AND UserRole = 53[Session+Diaconate]
+    '''):
+    print "  " + p.Name + "\n"
+    model.RemoveRole(p.PeopleId, "Session+Diaconate")
+    
+print "\n Add Session+Diaconate role\n"
+# Add the Session+Diaconate role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) = 1[True]
+    	OR IsMemberOf( Org=125[Diaconate] ) = 1[True]
+    )
+    AND UserRole <> 53[Session+Diaconate]
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Session+Diaconate")
+
+
+####### SESSION+WOMENSCMTE #######
+    
+print "\n Remove Session+WomensCmte role\n"
+# Remove the Session+WomensCmte role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) <> 1[True]
+    	AND IsMemberOf( Org=132[Women's Cmte] ) <> 1[True]
+    )
+    AND UserRole = 54[Session+WomensCmte]
+    '''):
+    print "  " + p.Name + "\n"
+    model.RemoveRole(p.PeopleId, "Session+WomensCmte")
+    
+print "\n Add Session+WomensCmte role\n"
+# Add the Session+WomensCmte role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) = 1[True]
+    	OR IsMemberOf( Org=132[Women's Cmte] ) = 1[True]
+    )
+    AND UserRole <> 54[Session+WomensCmte]
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Session+WomensCmte")
+    
+
+
+####### SESSION+DIACONATE+WOMENSCMTE #######
+    
+print "\n Remove Session+Diaconate+WomensCmte role\n"
+# Remove the Session+Diaconate+WomensCmte role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) <> 1[True]
+    	AND IsMemberOf( Org=125[Diaconate] ) <> 1[True]
+    	AND IsMemberOf( Org=132[Women's Cmte] ) <> 1[True]
+    )
+    AND UserRole = 55[Session+Diaconate+WomensCmte]
+    '''):
+    print "  " + p.Name + "\n"
+    model.RemoveRole(p.PeopleId, "Session+Diaconate+WomensCmte")
+    
+print "\n Add Session+Diaconate+WomensCmte role\n"
+# Add the Session+Diaconate+WomensCmte role for those appropriate. 
+for p in q.QueryList('''
+    (
+    	IsMemberOf( Org=124[Session] ) = 1[True]
+    	OR IsMemberOf( Org=125[Diaconate] ) = 1[True]
+    	OR IsMemberOf( Org=132[Women's Cmte] ) = 1[True]
+    )
+    AND UserRole <> 55[Session+Diaconate+WomensCmte]
+    '''):
+    print "  " + p.Name + "\n"
+    model.AddRole(p.PeopleId, "Session+Diaconate+WomensCmte")
