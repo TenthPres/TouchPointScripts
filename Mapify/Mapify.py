200a1f1a67e0656fa055eeea4029079d6e3d531a
James K <jkrrv@jkrrv.org>
2021-03-31 10:10:18 -0400

    Use XHR for data.  Allow base page to load faster.

--- Mapify/Mapify.py
+++ Mapify/Mapify.py
@@ -4,6 +4,12 @@ import urllib
 import json
 # noinspection PyUnresolvedReferences
 import clr
+clr.AddReference('System.Web.Extensions')
+clr.AddReference('System.Collections.Generic')
+
+# noinspection PyUnresolvedReferences
+from System.Web.Script.Serialization import JavaScriptSerializer
+
 # noinspection PyUnresolvedReferences
 from System.Collections.Generic import Dictionary, List
 
@@ -146,47 +152,17 @@ if model.FromMorningBatch or Data.action == "geocode":
 
     doAGeocode()
 
-elif model.Data.p1 == "":  # Blue Toolbar Page load
+elif model.Data.p == "" and model.Data.fams == "":  # Blue Toolbar Page load
 
     mapData = model.DynamicData()
     mapData.cesiumKey = model.Setting("CesiumKey", "")
     mapData.count = 0
 
-    try:  # TODO remove when PR 1370 is merged.
+    try:  # TODO remove try (and catch) when PR 1370 is merged.
         mapData.count = q.BlueToolbarCount()
     except:
-        print "<!-- WARNING: PR 1370 has not been merged yet -->"
+        print "<script>console.warn(\"WARNING: PR 1370 has not been merged yet.\");</script>"
         mapData.count = "unknown"
-
-    mapData.pts = Dictionary[str, Dictionary[str, object]]()
-    for p in q.BlueToolbarReport():
-        lat = model.ExtraValueText(p.PeopleId, geoLatEV)
-        lng = model.ExtraValueText(p.PeopleId, geoLngEV)
-        hsh = model.ExtraValueText(p.PeopleId, geoHashEV)
-
-        if lat == "" or lng == "":
-            continue
-
-        if not mapData.pts.ContainsKey(hsh):
-            mapData.pts.Add(hsh, Dictionary[str, object]({
-                'cnt': 0,
-                'hash': hsh,
-                'families': Dictionary[int, object](),
-                'addr': p.AddressLineOne or p.Family.AddressLineOne,
-                'lat': lat,
-                'lng': lng,
-            }))
-
-        if not mapData.pts[hsh]['families'].ContainsKey(p.Family.FamilyId):
-            mapData.pts[hsh]['families'][p.Family.FamilyId] = []
-
-        mapData.pts[hsh]['families'][p.Family.FamilyId].Add(p.PeopleId)
-        mapData.pts[hsh]['cnt'] += 1
-
-    mapData.ptsl = List[Dictionary[str, object]]()
-
-    for pi in mapData.pts:
-        mapData.ptsl.Add(pi.Value)
 
     template = """
 
@@ -197,7 +173,7 @@ elif model.Data.p1 == "":  # Blue Toolbar Page load
     <div id="cesiumContainer" class="fullSize"></div>
     <div id="loadingOverlay"><h2>Loading...</h2></div>
     <div id="toolbar"></div>
-    <div id="statusInfo" title="People who aren't shown probably could not be geocoded.">
+    <div id="statusInfo" title="People who aren't shown probably could not be located on the map.">
         Showing <span id="showingCount">0</span> of {{count}} people
     </div>
 
@@ -205,75 +181,88 @@ elif model.Data.p1 == "":  # Blue Toolbar Page load
     Cesium.Ion.defaultAccessToken = '{{{cesiumKey}}}';
     const viewer = new Cesium.Viewer('cesiumContainer', {
             animation : false,
-            fullscreenButton: false, // will be manually created later, to put it into the toolbar
+            fullscreenButton: false, // will be manually created later in the toolbar
             geocoder : false,
             infoBox: true,
             skyAtmosphere: false,
             skyBox: false,
             timeline : false,
             scene: {
+                fog: {
+                    enabled: false
+                },
                 globe: {
-                    enableLighting: true
+                    enableLighting: false,
+                    showGroundAtmosphere: false
                 }
             }
         });
 
-        //var data = Cesium.CzmlDataSource.load('/pythonapi/mapify?t=czml');
+    var flyHome = function() { 
+        viewer.camera.flyTo({ 
+            destination: Cesium.Cartesian3.fromDegrees(-75.169899, 39.947262, 85000.0), // TODO replace with church lat/lng setting 
+            duration: 4
+        });
+    };
 
-        var flyHome = function () { 
-            viewer.camera.flyTo({ 
-                destination: Cesium.Cartesian3.fromDegrees(-75.169899, 39.947262, 85000.0), // TODO replace with church lat/lng setting 
-                duration: 4
-            });
-        };
+    viewer.homeButton.viewModel._command = Cesium.createCommand(flyHome);
+    viewer.baseLayerPicker.viewModel.selectedImagery = viewer.baseLayerPicker.viewModel.imageryProviderViewModels[8];
+    viewer.infoBox.frame.sandbox = "allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts";
+    viewer.infoBox.viewModel.enableCamera = false;
+    viewer.scene.globe.showGroundAtmosphere = false;
 
-        entities = [];
+    // Prevent camera from getting locked to entity via double-click
+    viewer.cesiumWidget.screenSpaceEventHandler.setInputAction(function() {}, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
 
-        {{#each ptsl}}
-        entities.push(
-            viewer.entities.add({
-                position: Cesium.Cartesian3.fromDegrees({{this.lng}}, {{this.lat}}, 0),
-                name: "{{this.addr}}",
-                point: {
-                    pixelSize: Math.sqrt({{this.cnt}}) * 10,
-                    color: new Cesium.Color(0, 1, 0, 0.5)
-                },
-                _data: {
-                    hash: "{{this.hash}}",
-                    //famIds: {{this.families}}
-                }
-            })
-        );
-        {{/each}}
+    new Cesium.FullscreenButton(viewer._toolbar);
+    
+    let entities = {},
+        showCount = 0,
+        requestsNeeded = 1;
 
-        viewer.homeButton.viewModel._command = Cesium.createCommand(flyHome);
+    function reqListener() {
+        let start = this.responseText.indexOf(">>DATA>") + 7,
+            data = JSON.parse(this.responseText.substr(start, this.responseText.indexOf("<DATA<<") - start))
 
-        viewer.baseLayerPicker.viewModel.selectedImagery = viewer.baseLayerPicker.viewModel.imageryProviderViewModels[8];
-
-        viewer.infoBox.frame.sandbox = "allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts";
-        viewer.infoBox.viewModel.enableCamera = false;
-
-        // Prevent camera from getting locked to entity via double-click
-        viewer.cesiumWidget.screenSpaceEventHandler.setInputAction(function() {}, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
-
-        viewer.scene.fog.enabled = false
-        viewer.scene.globe.enableLighting = false;
-        viewer.scene.globe.showGroundAtmosphere = false;
-
-        new Cesium.FullscreenButton(viewer._toolbar);
-        
-        function reqListener () {
-            console.log(this.responseText);
+        for (const hsh in data) {
+            if (!entities.hasOwnProperty(hsh)) {
+                entities[hsh] = viewer.entities.add({
+                    position: Cesium.Cartesian3.fromDegrees(data[hsh].lng, data[hsh].lat, 0),
+                    name: data[hsh].addr,
+                    point: {
+                        pixelSize: Math.sqrt(data[hsh].cnt) * 10,
+                        color: new Cesium.Color(0, 1, 0, 0.5)
+                    },
+                    _data: {
+                        hash: hsh,
+                        famIds: data[hsh].families
+                    }
+                });
+                showCount += data[hsh].cnt;
+            } else {
+                console.log("ERROR: hash points need to be merged across requests."); // TODO
+            }
         }
-        
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", reqListener);
-        xhr.open("GET", "/PyScript/Mapify/Jawn");
-        xhr.send();
 
-        flyHome();
-        
-        document.getElementById('loadingOverlay').style.opacity = 0;
+        // Update count of number of people shown
+        document.getElementById("showingCount").innerHTML = "" + showCount;
+
+        // Fully Loaded
+        requestsNeeded--
+        if (requestsNeeded <= 0) {
+            flyHome();
+            document.getElementById('loadingOverlay').style.opacity = 0;
+        }
+
+    }
+
+    for (var r = 1; r <= requestsNeeded; r++) {
+        let url = window.location.origin + window.location.pathname + "?p=" + r,
+            xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", reqListener);
+        xhr.open("GET", url);
+        xhr.send();
+    }
 
     </script>
     """
@@ -281,6 +270,34 @@ elif model.Data.p1 == "":  # Blue Toolbar Page load
     print model.RenderTemplate(template, mapData)
 
 
-elif model.Data.p1 is not None:  # XHR Data Request
+elif model.Data.p != "":  # XHR Data Request
 
-    print "{}"
+    pts = Dictionary[str, Dictionary[str, object]]()
+    for p in q.BlueToolbarReport():
+        lat = model.ExtraValueText(p.PeopleId, geoLatEV)
+        lng = model.ExtraValueText(p.PeopleId, geoLngEV)
+        hsh = model.ExtraValueText(p.PeopleId, geoHashEV)
+
+        if lat == "" or lng == "":
+            continue
+
+        if not pts.ContainsKey(hsh):
+            pts.Add(hsh, Dictionary[str, object]({
+                'cnt': 0,
+                'hash': hsh,
+                'families': Dictionary[str, object](),
+                'addr': p.AddressLineOne or p.Family.AddressLineOne,
+                'lat': lat,
+                'lng': lng,
+            }))
+
+        if not pts[hsh]['families'].ContainsKey(str(p.Family.FamilyId)):
+            pts[hsh]['families'][str(p.Family.FamilyId)] = []
+
+        pts[hsh]['families'][str(p.Family.FamilyId)].Add(p.PeopleId)
+        pts[hsh]['cnt'] += 1
+
+    print "<!-->>>>>DATA>" + JavaScriptSerializer().Serialize(pts) + "<DATA<<<<<-->"
+
+elif model.Data.fams != "":  # XHR Data Request - Families and people TODO: this.
+    print "<!-->>>>>DATA>" + JavaScriptSerializer().Serialize([]) + "<DATA<<<<<-->"
