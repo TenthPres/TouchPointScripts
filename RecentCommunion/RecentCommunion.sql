738395e14825c00b20e48b14824ececa26de86a5
James K <jkurtz@tenth.org>
2022-02-13 11:21:50 -0500

    Simply and correct the math for consistency

--- RecentCommunion/RecentCommunion.sql
+++ RecentCommunion/RecentCommunion.sql
@@ -1,7 +1,7 @@
-SELECT a.PeopleId
+SELECT DISTINCT a.PeopleId
 FROM Attend AS a
 JOIN MeetingExtra AS me ON a.MeetingId = me.MeetingId
 WHERE a.AttendanceFlag = 1
   AND LOWER(me.Field) = 'communion'
   AND me.data = '1'
-  AND MeetingDate >= Dateadd(MONTH, Datediff(MONTH, 0, DATEADD(MONTH, -3, CURRENT_TIMESTAMP)), 0)
+  AND MeetingDate >= DATEADD(DAY, -90, CURRENT_TIMESTAMP)
