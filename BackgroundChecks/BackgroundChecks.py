596347a822ff5f8accc86b45623295f1d75ddf73
James K <jkrrv@jkrrv.org>
2022-12-12 10:51:07 -0500

    Updating PA Background check situation

--- BackgroundChecks/BackgroundChecks.py
+++ BackgroundChecks/BackgroundChecks.py
@@ -2,7 +2,7 @@
 from System import DateTime, String, Convert
 # noinspection PyUnresolvedReferences
 from System.Text.RegularExpressions import Regex, RegexOptions
-from pprint import pprint
+#from pprint import pprint
 
 global q, model  # This line isn't necessary, but helps with using an IDE.
 
@@ -11,6 +11,9 @@ global q, model  # This line isn't necessary, but helps with using an IDE.
 checkCode = "ComboPS"
 employeeOrgId = 61
 stateCode = "PA"
+
+affidFlowTriggerUrl = "https://prod-02.westus.logic.azure.com:443/workflows/2f05980be1e64670b6b38cf705a85713/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DIJ7e0sGiMjDyL_7EZRjhgR6mS7zK-ef65GAANZ_iZM"
+affidEv = "ds-Resident Affidavit"
 
 people_needing_checks = '''
     IsProspectOf( Org={0} ) = 1[True]
@@ -241,7 +244,7 @@ class BackgroundChecker:
             self.statusExp['isMin'] = self.Statuses['CHECK_STARTED'] | self.Statuses['CHECK_COMPLETE'] | \
                                       self.Statuses['REVIEW_COMPLETE'] | self.Statuses['PASSED']
 
-        # FBI Fingerprinting
+        # Manual Uploads of documents
         for doc in self.person.VolunteerForms:
             rx = Regex("(?<docType>\w+)\s+(?<date>[0-9]{1,2}/[0-9]{1,2}/[0-9]{4})",
                        RegexOptions.Compiled | RegexOptions.IgnoreCase)
@@ -250,6 +253,7 @@ class BackgroundChecker:
             if not rm.Success:
                 continue
 
+            # FBI Fingerprinting
             if rm.Groups['docType'].Value.ToLower() == "fbi":
                 dt = Convert.ToDateTime(rm.Groups['date'].Value)
 
@@ -264,6 +268,22 @@ class BackgroundChecker:
 
                 if DateTime.Compare(dt, self.Expirations['fingr']) > 0:
                     self.statusCur['fingr'] = self.statusCur['fingr'] | check_status
+                    
+            # Resident Affidavit
+            if rm.Groups['docType'].Value.ToLower() == "affid":
+                dt = Convert.ToDateTime(rm.Groups['date'].Value)
+
+                # TODO impose some form of verification here.
+                check_status = self.Statuses['CHECK_STARTED'] | self.Statuses['CHECK_COMPLETE'] | \
+                               self.Statuses['REVIEW_COMPLETE'] | self.Statuses['PASSED']
+
+                self.statusHis['affid'] = self.statusHis['affid'] | check_status
+
+                if DateTime.Compare(dt, self.Renewals['affid']) > 0:
+                    self.statusExp['affid'] = self.statusExp['affid'] | check_status
+
+                if DateTime.Compare(dt, self.Expirations['affid']) > 0:
+                    self.statusCur['affid'] = self.statusCur['affid'] | check_status
 
         self.statusSet = True
 
@@ -395,7 +415,44 @@ elif model.Data.view == "list" and userPerson.Users[0].InRole('BackgroundCheck')
             print "<p><i>No Documents</i></p>"
 
         print "</div>"
+        
 
+elif model.Data.view == "affid":
+    model.Header = "PA Resident Affidavit"
+    
+    if model.Data.viewas != "" and userPerson.Users[0].InRole('BackgroundCheck'):
+        bgc = BackgroundChecker(int(model.Data.viewas))
+    else:
+        bgc = BackgroundChecker()
+        
+    agreementEnv = model.ExtraValueText(bgc.person.PeopleId, affidEv)
+    
+    if model.InOrg(bgc.person.PeopleId, employeeOrgId):
+        print "<p>As you are an employee, you are not eligible for the affidavit.  Please go back to complete the process.</p>"
+    
+    elif agreementEnv is None or agreementEnv == '':
+        jsonData = {
+            "subject": {
+                "firstName": bgc.person.FirstName,
+                "lastName": bgc.person.LastName,
+                "email": bgc.person.EmailAddress,
+                "pid": bgc.person.PeopleId
+            }
+        }
+        
+        agreement = model.RestPostJson(affidFlowTriggerUrl, {}, jsonData)
+        
+        agreement = model.JsonDeserialize(agreement)
+            
+        if (agreement['subject']['pid'] == bgc.person.PeopleId):
+            model.AddExtraValueText(bgc.person.PeopleId, affidEv, agreement['envelopeId'])
+            
+            print "<p>In a moment, you should recieve an email from DocuSign to certify your residency in Pennsylvania.  Please complete the form to complete this stage of the background check.</p>"
+        else:
+            print "<p>An error occurred: PID mismatch</p>"
+            
+    else:
+        print "<p>It appears that you already have an affidavit in progress or on file.</p>"  # TODO clarify this
 
 elif model.HttpMethod == "get":
 
@@ -478,7 +535,7 @@ elif model.HttpMethod == "get":
             print "<tr><td><label for=\"ssn\">SSN</label></td><td><input type=\"password\" id=\"ssn\" " \
                   "required=\"required\" name=\"ssn\" placeholder=\"000-00-0000\" maxlength=\"12\" pattern=\"[0-9]{" \
                   "3}[-]?[0-9]{2}[-]?[0-9]{4}\" /></td></tr> "
-            print "<tr><td></td><td><input type=\"submit\" /></td></tr>"
+            print "<tr><td></td><td><input type=\"submit\" class=\"btn btn-primary\" /></td></tr>"
             print "</tbody></table>"
 
             print "</form>"
@@ -487,7 +544,7 @@ elif model.HttpMethod == "get":
         elif 'submit_emp' in to_do or 'submit_vol' in to_do or 'submit_basic' in to_do:
             print "<div class=\"well\">"
             
-            pprint(to_do)
+            # pprint(to_do)
             
             print "<form method=\"POST\" action=\"/PyScriptForm/{}\">".format(model.ScriptName)
             print "<table><tbody>"
@@ -507,8 +564,6 @@ elif model.HttpMethod == "get":
             print "</div>"
 
         if 'submit_fbi_aff' in to_do:
-            print "<form method=\"POST\" enctype=\"multipart/form-data\" action=\"/PyScriptForm/{}?set=fbi\">".format(
-                model.ScriptName)
             print "<div class=\"well\">We need your FBI Fingerprinting clearance or an affidavit.  <br />"
             print "If you <b>have only lived in Pennsylvania within the last 10 years</b>, " \
                   "<a href=\"?view=affid\">please click here to sign an affidavit</a>.<br /> "
@@ -518,11 +573,9 @@ elif model.HttpMethod == "get":
                   "appointment.</a>  Pennsylvania uses IdentoGo as a provider for this service.  Either make your " \
                   "appointment at a location in PA (suggested), or use the \"Card Submission By Mail\" option, " \
                   "which will provide instructions for completing a fingerprint card and submitting it back to " \
-                  "IdentoGo. Once you receive your certification in the mail, please scan it and upload it here. "
-            print "<!--<input type=\"file\" name=\"fbi\" />-->"
-            print "<!--<input type=\"submit\" />-->"
+                  "IdentoGo. Once you receive your certification in the mail, please scan it and "
+            print "<a href=\"/OnlineReg/96\" >upload it here</a>."
             print "</div>"
-            print "</form>"
 
         if 'submit_fbi' in to_do:
             print "<div class=\"well\">We need your FBI Fingerprinting clearance.  " \
@@ -530,7 +583,9 @@ elif model.HttpMethod == "get":
                   " target=\"_blank\">Click here to enter your information and arrange a fingerprinting " \
                   "appointment.</a>  Pennsylvania uses IdentoGo as a provider for this service, and you will be " \
                   "required to make an appointment at a location in PA.  A few weeks after your appointment, "\
-                  " your certification will be mailed to you.  Please scan it and upload it here. </div>"
+                  " your certification will be mailed to you.  Please scan it and upload it here."
+            print "<br /><a href=\"/OnlineReg/96\" class=\"btn btn-primary\">Submit Document</a>"
+            print "</div>"
 
 
 elif model.HttpMethod == "post":
@@ -554,27 +609,44 @@ elif model.HttpMethod == "post":
         ssn = model.Data.ssn
 
     package = None
+    needsPmmSupport = False
 
     if 'submit_emp' in to_do:
-        submit = ''
-        package = 'PA Employee'
+        #Bron: old way
+        #submit = ''
+        #package = 'PA Employee'
+        submit = 'PA Employee'
+        needsPmmSupport = True
 
     elif 'submit_vol' in to_do:
         submit = 'ComboPS'
+        needsPmmSupport = True
 
     elif 'submit_basic' in to_do:
         submit = 'Combo'
 
     user = model.Setting('PMMUser')  # type: str
     pasw = model.Setting('PMMPassword')  # type: str
+    
+    if forEmployment == 1:
+        package = "PA Employee"
+    else:
+        package = "PA Package"
 
     if submit is not False and ssn is not False:
-        submit = model.AddBackgroundCheck(bgc.person.PeopleId, submit, 1, forEmployment, ssn, sUser=user,
-                                          sPassword=pasw, sPlusState=stateCode, sPackageName=package)
+        submit = model.AddBackgroundCheck(bgc.person.PeopleId, package, sSSN=ssn)
 
     elif submit is not False and ssn is False:
-        submit = model.AddBackgroundCheck(bgc.person.PeopleId, submit, 1, forEmployment, sUser=user, sPassword=pasw,
-                                          sPlusState=stateCode, sPackageName=package)
+        submit = model.AddBackgroundCheck(bgc.person.PeopleId, package)
+                                          
+    if needsPmmSupport and isinstance(submit, int):
+        pmmId = q.QuerySqlInt("SELECT TOP 1 ReportId FROM BackgroundChecks WHERE id = {}".format(submit))
+        if pmmId is not None:
+            model.Transactional = True
+            # model.TestEmail = True  TODO remove or reimplement.
+            #emailSubj = "PA Child Abuse Check for Report {} - Tenth Presbyterian Church".format(pmmId)
+            #emailBody = "Report {2} requires a PA Child Abuse Check for {0} {1}.  Please ensure this is applied correctly and that clearances@tenth.org is cc'd on any emails sent to {0}.".format(FirstName, LastName, pmmId)
+            #model.Email(27950, 12255, "jamesk@tenth.org", "James Kurtz - Tenth Presbyterian Church", emailSubj, emailBody)
 
     print "REDIRECT={}/PyScript/{}".format(model.CmsHost, model.ScriptName)
 
