60911349139ba3d4810634b2e10f19830e3f6c58
James K <jkrrv@jkrrv.org>
2022-12-12 10:58:39 -0500

    Updating Registrations in Progress to be more user-friendly

--- RegistrationsInProgress/RegistrationsInProgress.sql
+++ RegistrationsInProgress/RegistrationsInProgress.sql
@@ -1,2 +1,10 @@
-SELECT TOP 100 * FROM InProgressRegistrations 
-ORDER BY Stamp DESC
+SELECT TOP 100 
+    ipr.OrganizationName,
+    ipr.Stamp,
+    ipr.PeopleId,
+    ipr.OrganizationId,
+    ipr.RegDataId,
+    p.name
+FROM InProgressRegistrations ipr
+    JOIN People p on ipr.PeopleId = p.PeopleId
+ORDER BY ipr.Stamp DESC
