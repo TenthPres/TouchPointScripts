2bb0371a1af1b214e4ac5b5d07963545c5a05ebc
James K <jkrrv@jkrrv.org>
2023-02-06 14:56:58 -0500

    Updating CloseOnlineBundles

--- CloseOnlineBundles/CloseOnlineBundles.py
+++ CloseOnlineBundles/CloseOnlineBundles.py
@@ -1,9 +1,12 @@
 import clr
 from System import DateTime
 
-sql = "SELECT TOP 100 ContributionDate FROM BundleHeader WHERE BundleHeaderTypeId = 4 AND BundleStatusId = 1 AND ContributionDate < DateAdd(d, -4, getdate())"
+daysAgo = 2
+
+sql = "SELECT TOP 100 ContributionDate FROM BundleHeader WHERE BundleHeaderTypeId = 4 AND BundleStatusId = 1 AND ContributionDate < DateAdd(d, -{}, getdate())".format(daysAgo)
 
 for s in q.QuerySql(sql):
-    bh = model.FindBundleHeader(s.ContributionDate)
-    model.FinishBundle(bh)
-    model.CloseBundle(bh)
+    bh = model.FindBundleHeader(s.ContributionDate, 'Online')
+    if bh is not None:
+        model.FinishBundle(bh)
+        model.CloseBundle(bh)
