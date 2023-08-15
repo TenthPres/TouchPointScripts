a222cf3546dd322a5d9c2c90b913628b321dd7ef
James K <jkrrv@jkrrv.org>
2023-08-15 14:24:16 -0400

    automating build/release procedure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp'),
 	zip = require('gulp-zip');
 
-exports.default = () => (
+function buildEverything(cb) {
 
 	// Mapify Only
 	gulp.src(['Mapify/*.py', "Mapify/keyword"])
 		.pipe(zip('Mapify.zip'))
-		.pipe(gulp.dest('.Builds'))
-);
+		.pipe(gulp.dest('.Builds'));
+
+	// Tuition Only
+	gulp.src(['TuitionAutomation/*.py', "TuitionAutomation/keyword", "TuitionAutomation/*.sql"])
+		.pipe(zip('TuitionAutomation.zip'))
+		.pipe(gulp.dest('.Builds'));
+
+	cb();
+}
+
+exports.default = buildEverything;
