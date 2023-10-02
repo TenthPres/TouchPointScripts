d6b56e0d3f4e4a075ca1f50aca95234ee8cc0491
James K. <jkrrv@jkrrv.org>
2023-10-02 13:24:50 -0400

    Updating Intacct Export

--- SageIntacct/ExportForIntacct.sql
+++ SageIntacct/ExportForIntacct.sql
@@ -2,55 +2,89 @@
 --class=TotalsByFund
 
 DECLARE @ProjectOfMonthFund AS int;
+DECLARE @RegistrationIncomeFund AS int;
 
 /* If you have a "Project of the Month" fund where different months are identified as separate projects, put the Project of the Month Fund ID here */
 SET @ProjectOfMonthFund = 25;
 
-SELECT 
-    bd.BundleHeaderId AS BundleId,
+-- Donations, except Project of the Month
+SELECT
+    t.BatchRef AS BatchId,
     bht.Code as Typ,
-    bh.ContributionDate AS BundContribDate,
-    c.FundId, 
-    cf.FundName AS Name, 
+    CONVERT(VARCHAR, c.ContributionDate, 101) AS ContribDate,
+    CONVERT(VARCHAR, t.Settled, 101) as SettleDate,
+    c.FundId,
+    cf.FundName AS Name,
     SUM(c.ContributionAmount) AS Amt,
-    cf.FundIncomeFund * 1 AS Account,
-    cf.FundIncomeAccount * 1 AS IncomeAccount,
+    cf.FundAccountCode * 1 AS Project,
+    cf.FundIncomeFund * 1 AS Fund,
     cf.FundIncomeDept * 1 as Department,
-    cf.FundAccountCode * 1 AS Project
+    cf.FundIncomeAccount * 1 AS GL
 FROM Contribution AS c
+         LEFT JOIN [Transaction] as t ON c.TranId = t.Id
     LEFT JOIN ContributionFund AS cf on c.FundId = cf.FundId
     LEFT JOIN BundleDetail AS bd on bd.ContributionId = c.ContributionId
     LEFT JOIN BundleHeader AS bh on bd.BundleHeaderId = bh.BundleHeaderId
     LEFT JOIN lookup.BundleHeaderTypes AS bht on bht.Id = bh.BundleHeaderTypeId
 WHERE c.FundId <> @ProjectOfMonthFund
-    AND bh.ContributionDate between @StartDate AND @EndDate
-    AND bh.BundleStatusId = 0
-GROUP BY bd.BundleHeaderId, bht.Code, bh.ContributionDate, c.FundId, cf.FundName, cf.FundAccountCode, cf.FundIncomeAccount, cf.FundIncomeDept, cf.FundIncomeFund
+  AND c.ContributionTypeId <> 99
+  AND bh.ContributionDate between @StartDate AND @EndDate
+  AND bh.BundleStatusId = 0
+GROUP BY t.BatchRef, bht.Code, CONVERT(VARCHAR, c.ContributionDate, 101), CONVERT(VARCHAR, t.Settled, 101), c.FundId, cf.FundName, cf.FundAccountCode, cf.FundIncomeAccount, cf.FundIncomeDept, cf.FundIncomeFund
 
 UNION ALL
 
-SELECT 
-    bd.BundleHeaderId as BundleId,
-    bht.Code as Typ,
-    bh.ContributionDate AS BundContribDate,
-    c.FundId, 
-    CONCAT(cf.FundName, ' (', FORMAT(c.ContributionDate, 'Y', 'en-US'), ')' ) AS Name, 
+-- Registrations
+SELECT
+    t.BatchRef AS BatchId,
+    CONCAT('R-', bht.Code) as Typ,
+    CONVERT(VARCHAR, c.ContributionDate, 101) AS ContribDate,
+    CONVERT(VARCHAR, t.Settled, 101) as SettleDate,
+    null as fundId,
+    o.OrganizationName AS Name,
     SUM(c.ContributionAmount) AS Amt,
-    cf.FundIncomeFund * 1 as Account,
-    cf.FundIncomeAccount * 1 as IncomeAccount,
-    cf.FundIncomeDept * 1 as Department,
-
-/* The Project code for the "Project of the Month" is computed on the next line */
-    MIN(DATEPART(yy, c.ContributionDate) * 100 + DATEPART(mm, c.ContributionDate)) AS Project
-
+    TRY_PARSE(SUBSTRING(oe.Data, 1, 4) as int) AS Project,
+    TRY_PARSE(SUBSTRING(oe.Data, 6, 3) as int) AS Fund,
+    TRY_PARSE(SUBSTRING(oe.Data, 10, 4) as int) AS Department,
+    TRY_PARSE(SUBSTRING(oe.Data, 15, 5) as int) AS GL
 FROM Contribution AS c
+         LEFT JOIN [Transaction] as t ON c.TranId = t.Id
+    LEFT JOIN OrganizationExtra as oe ON t.OrgId = oe.OrganizationId AND oe.Field = 'SI Coding'
+    LEFT JOIN Organizations as o ON t.OrgId = o.OrganizationId
+    LEFT JOIN ContributionFund AS cf on oe.IntValue = cf.FundId
+    LEFT JOIN BundleDetail AS bd on bd.ContributionId = c.ContributionId
+    LEFT JOIN BundleHeader AS bh on bd.BundleHeaderId = bh.BundleHeaderId
+    LEFT JOIN lookup.BundleHeaderTypes AS bht on bht.Id = bh.BundleHeaderTypeId
+WHERE c.FundId <> @ProjectOfMonthFund
+  AND c.ContributionTypeId = 99
+  AND bh.ContributionDate between @StartDate AND @EndDate
+  AND bh.BundleStatusId = 0
+GROUP BY t.BatchRef, bht.Code, CONVERT(VARCHAR, c.ContributionDate, 101), CONVERT(VARCHAR, t.Settled, 101), oe.Data, o.OrganizationName
+
+UNION ALL
+
+-- Project of the Month
+SELECT
+    t.BatchRef as BatchId,
+    bht.Code as Typ,
+    CONVERT(VARCHAR, c.ContributionDate, 101) AS ContribDate,
+    CONVERT(VARCHAR, t.Settled, 101) as SettleDate,
+    c.FundId,
+    CONCAT(cf.FundName, ' (', FORMAT(c.ContributionDate, 'Y', 'en-US'), ')' ) AS Name,
+    SUM(c.ContributionAmount) AS Amt,
+    MIN(DATEPART(yy, c.ContributionDate) * 100 + DATEPART(mm, c.ContributionDate)) AS Project,
+    cf.FundIncomeFund * 1 AS Fund,
+    cf.FundIncomeDept * 1 as Department,
+    cf.FundIncomeAccount * 1 AS GL
+FROM Contribution AS c
+         LEFT JOIN [Transaction] as t ON c.TranId = t.Id
     LEFT JOIN ContributionFund AS cf on c.FundId = cf.FundId
     LEFT JOIN BundleDetail AS bd on bd.ContributionId = c.ContributionId
-    LEFT JOIN BundleHeader AS bh on bd.BundleHeaderId = bh.BundleHeaderId 
-    LEFT JOIN lookup.BundleHeaderTypes AS bht on bht.Id = bh.BundleHeaderTypeId 
+    LEFT JOIN BundleHeader AS bh on bd.BundleHeaderId = bh.BundleHeaderId
+    LEFT JOIN lookup.BundleHeaderTypes AS bht on bht.Id = bh.BundleHeaderTypeId
 WHERE c.FundId = @ProjectOfMonthFund
-    AND bh.ContributionDate between @StartDate AND @EndDate
-    AND bh.BundleStatusId = 0
-GROUP BY bd.BundleHeaderId, bht.Code, bh.ContributionDate, c.FundId, FORMAT(c.ContributionDate, 'Y', 'en-US'), cf.FundName, cf.FundAccountCode, cf.FundIncomeAccount, cf.FundIncomeDept, cf.FundIncomeFund
+  AND bh.ContributionDate between @StartDate AND @EndDate
+  AND bh.BundleStatusId = 0
+GROUP BY t.BatchRef, bht.Code, CONVERT(VARCHAR, c.ContributionDate, 101), CONVERT(VARCHAR, t.Settled, 101), c.FundId, FORMAT(c.ContributionDate, 'Y', 'en-US'), cf.FundName, cf.FundAccountCode, cf.FundIncomeAccount, cf.FundIncomeDept, cf.FundIncomeFund
 
-ORDER BY BundContribDate ASC, BundleId ASC, FundId ASC
+ORDER BY ContribDate ASC, Typ ASC, BatchId ASC, FundId ASC
--- SageIntacct/README.md
+++ SageIntacct/README.md
@@ -1,30 +1,34 @@
 # Export for Intacct
 
-This script exports contributions in a format requested by our accountants (AcctTwo), for importing into Sage Intaact for 
-bank deposit reconciliations.  It is among the "Totals By Fund" reports. 
+This script exports contributions and registrations in a format requested by our accountants (Baker Tilly), for 
+importing into Sage Intacct for bank deposit reconciliations.  It is among the "Totals By Fund" reports. 
 
-TouchPoint fields are mapped to columns in this report as follows: 
-    IncomeFund AS Account,
-    IncomeAccount AS IncomeAccount,
-    IncomeDept AS Department,
-    AccountCode AS Project
+TouchPoint fields for Contributions are mapped to columns in this report as follows:
+    FundAccountCode AS Project,
+    FundIncomeFund AS Fund,
+    FundIncomeDept AS Department,
+    FundIncomeAccount AS GL
+
+For Registrations, the Involvement must have a text Extra Value called 'SI Coding' and it must match the format:
+`pppp-fff-dddd-ggggg` where pppp is a 4-digit Project code, fff is a 3-digit fund code, dddd is a 4-digit department 
+code, and ggggg is a 5-digit GL code.
 
 ### Project of the Month
 
-We have a "Project of the Month." We decided to use a single TouchPoint fund for this, which allows donors to setup recurring giving to
-the "Project of the Month", whatever that month may be. However, since the projects change each month, we needed a different
-Project code for Intacct each month. 
+We have a "Project of the Month." We decided to use a single TouchPoint fund for this, which allows donors to set up 
+recurring giving to the "Project of the Month", whatever that month may be. However, since the projects change each 
+month, we needed a different Project code for Intacct each month. 
 
-To accommodate this, the Project Code for the Project of the month is calculated based on the contribution date as YYYYMM, so
-August 2021 would be project 202108.  This can be replaced with some other system by changing the math on 
-[line 44](ExportForIntacct.sql#L44). 
+To accommodate this, the Project Code for the Project of the month is calculated based on the contribution date as 
+YYYYMM, so August 2021 would be project 202108.  This can be replaced with some other system by changing the math on 
+[line 75](ExportForIntacct.sql#L75). 
 
 If you don't have a project of the month, you can safely set `@ProjectOfMonthFund` to 0 and that feature will not run. 
 
 ## How to Install
 
 1. In the Special Content section of TouchPoint, create a new SQL Script called `ExportForIntacct`.  
-1. Paste into that document all of the content from [ExportForIntacct.sql](ExportForIntacct.sql).  
+1. Paste into that document all the content from [ExportForIntacct.sql](ExportForIntacct.sql).  
 1. Change the Project of the Month Fund Id to your Project of the Month fund if you have one, or set to 0 if you don't have one. 
 1. Save the file.
 
