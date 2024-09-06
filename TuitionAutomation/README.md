f94b7e012bb4841ea23b33d9ae5a9f9e62f747ec
James K. <jkrrv@jkrrv.org>
2024-09-05 21:49:06 -0400

    This year's tuition script updates, so far.

--- TuitionAutomation/README.md
+++ TuitionAutomation/README.md
@@ -1,73 +1,84 @@
 # Tuition Automation
 
-This is a tool intended to automatically bill monthly tuition for students in our Preschool program.  This is a function
+This is a tool intended for billing monthly tuition for students in our Preschool program.  This is a function
 that TouchPoint is *definitely not* built for, so while this solution is  **NOT** an ideal solution, it is a viable
 solution and provides the level of flexibility we need.  However, if you want a more capable system, you should probably
 look to a true Learning/School/Preschool management system.
 
 ## Limitations
-- **Cannot set up automatic recurring payments.**  That's just not a thing that TouchPoint can do.
-- **Requires a separate PayLink for each child.**  So, if multiple children are enrolled, the parent will need to make
-  two separate payments each billing cycle.
+- **Cannot set up automatic recurring payments.**  (That is, payments without user intervention.)  That's just not a thing that TouchPoint can currently do.
+- **Sometimes requires a separate PayLink for each child.**  If parents register multiple children at the same time through a TouchPoint registration, they can share a paylink.  Otherwise, you'll end up with a separate paylink per child.
 
 ## Features
-- **Automatic Discounts** based on attributes in the database.  For example, we have a discount for church members.  If
-  parents become members during the school year, the discount automatically starts applying when they become members.
+- **Automatic Discounts** based on attributes in the database.  For example, we have a discount for church members.  If parents become members during the school year, the discount automatically starts applying once they become members.
 - **Manual Override option** for students who may be on scholarship or financial aid.
-- **Unified accounts** for each child across related programs, so the main program and aftercare can be billed/payed together.
+- **Unified accounts** for each child across related programs, so the main program and aftercare can be billed/paid together.
 - **Automated-ish Billing emails** with the account history and balance.
 
 ## Setup
 1.  Download the TuitionAutomation zip file from the releases and install it at `mychurch.tpsdb.com/InstallPyScriptProject`.
     This will install the scripts, but you will need to customize them.
-2.  Create the payment involvement, and also the involvements for each class.  The classes can be child involvements of
-    the payment involvement, but this is not required. Take note of the Involvement IDs for all of these involvements.
-   3.  Go to Special Content > Python and edit the TuitionAutomation script. Towards the top, you'll see a few variables
-       that you'll need to customize: 
-       ```python
-       paymentOrgId = 51
-       minTransactionId = 17000
-       ```
-       The `paymentOrgId` is the Involvement ID of the payment involvement.
-    
-       The `minTransactionId` is a means of filtering out older transactions, which is particularly useful if you reuse involvements year to year.
-    
-       Then, there's a section where each class's parameters are spelled out:
-       ```python
-       orgs = [
-           {
-               'id': 209,
-               'name': "2-Year-Old",
-               'base': 332
-           },
-           {
-               'id': 215,
-               'name': "Afternoon - Mon",
-               'base': 40,
-               'payPer': "meeting",
-               'discounts': False
-           },
-           ...
-       ]
-       ```
-       Each set of {} brackets goes with a single class.  You'll need one of those for each class.  **Important**: 
-       billing is processed **in order** along this list, starting at the top.  So, if you want to have a "younger sibling" 
-       discount, make sure
-       you put these in order with older classes listed first. 
-    
-       `id` is the Involvement ID of the class. 
+2.  Create an involvement which will be the "payment involvement" in the rest of this documentation.  If you want families to register online, including paying a deposit, this is the involvement where you should put the registration form. 
+3.  Create involvements for each class (hereafter, "class involvements").  You will need to add the students as members of these involvements.  We recommend also adding the teachers as leaders, so they can take attendance.   The class involvements can be child involvements of
+    the payment involvement, but this is not required. Take note of the Involvement IDs for all of these involvements. 
+4.  Go to Special Content > Python and edit the TuitionAutomation script. Towards the top, you'll see a few variables that you'll need to customize:
+    ```python
+    paymentOrgId = 51
+    minTransactionId = 17000
+    ```
+    The `paymentOrgId` is the Involvement ID of the payment involvement.
 
-       `name` is the name of the Involvement, as you want it to appear on billing statements and transactions. 
+    The `minTransactionId` is a means of filtering out older transactions, which is particularly useful if you reuse involvements year to year.  (We recommend alternating involvements year to year, so one involvement is the current year, while the other is for next year.  More on transitioning between years is below.)
 
-       `base` is the amount of tuition to charge.  By default, this is per-month.  
+    Then, there's a section where each class's parameters are spelled out:
+    ```python
+    orgs = [
+        {
+            'id': 209,
+            'name': "2-Year-Old",
+            'base': 332
+        },
+        {
+            'id': 215,
+            'name': "Afternoon - Mon",
+            'base': 40,
+            'payPer': "meeting",
+            'discounts': False
+        },
+        ...
+    ]
+    ```
+    Each set of {} brackets goes with a single class.  You'll need one of those for each class involvement.  **Important**:
+    billing is processed **in order** along this list, starting at the top.  So, if you want to have a "younger sibling"
+    discount, make sure you put these in order with older classes listed first.
 
-       `payPer` allows for switching to per-meeting billing, rather than per-month.  If you're sticking with the default of per-month, you can remove this field.
+    `id` is the Involvement ID of the class involvement.
 
-       `discounts` can be set false to disallow discounts, such as the sibling discount and church member discount.
+    `name` is the name of the Involvement, as you want it to appear on billing statements and transactions.
+
+    `base` is the amount of tuition to charge.  By default, this is per-month.
+
+    `payPer` allows for switching to per-meeting billing, rather than per-month.  If you're sticking with the default of per-month, you can remove this field.  (Our aftercare program is billed per-meeting for which the student is enrolled.)
+
+    `discounts` can be set false to disallow discounts, such as the sibling discount and church member discount. (Our aftercare program does not allow discounts.)
+5.  Once you've customized the script, you can run it from the Python page.  This will assess the tuition for each student in each class involvement.  This initial run will make sure all students enrolled in the classes are also members of the payment involvement. 
+6.  If you need to provide special discounts or scholarships, you can do so by adding a note to the student's involvement membership **in the class involvement**.  To do this, go to the class involvement > People and click the pencil icon for the student.  Then, on the Extra Value tab, add an Extra Value of type "Integer" named "Tuition" with the value being the amount of tuition to bill, replacing the amount defined in the script as described above.  Only whole-dollar positive amounts are supported.
 
 ## Usage
+Once the setup steps are complete, navigate to /PyScript/TuitionAutomation from which you'll be able to access most of
+the features available to you.
 
-Once the setup steps are complete, navigate to /PyScript/TuitionAutomation from which you'll be able to access most of 
-the features available to you.  
+## Between School Years
 
-(Much more to be written...)
+You can only handle one school year at a time in a payment involvement.
+
+We have two payment involvements, one for the current year and one for the next year.  This allows us to transition between years without losing the history of the previous year. 
+Mid-way through the year, when we open registration for the next year, we configure registration in the non-current payment involvement, where parents can enroll and pay the deposits for their children.  The deposit amounts are just fees in the registration and are handled in the normal way.  If you don't want to charge fees at enrollment, you don't have to.
+
+During the summer:
+- Update class involvements to reflect the new class lists. Make sure teachers are added as leaders in the involvements.
+- Change the Payment Involvement ID to a different one from the previous year. 
+- If needed, update the minimum transaction ID in the script to filter out old transactions.
+- Update the script to reflect the new class involvements (if changed) and tuition amounts.
+- ALWAYS double-check the assessed amounts BEFORE applying them to accounts.
+
--- TuitionAutomation/TuitionAutomation.py
+++ TuitionAutomation/TuitionAutomation.py
@@ -6,44 +6,45 @@ import urllib
 # model.TestEmail = True
 model.Transactional = True
 
+
 orgs = [
     {
         'id': 214,
         'name': "Kindergarten",
-        'base': 837
+        'base': 885
     },
-    
+
     {
         'id': 212,
         'name': "Pre-K",
-        'base': 837
+        'base': 870
     },
     {
         'id': 213,
         'name': "Pre-K",
-        'base': 837
+        'base': 870
     },
-    
+
     {
         'id': 210,
         'name': "3-Year-Old",
-        'base': 497
+        'base': 520
     },
     {
         'id': 211,
         'name': "3-Year-Old",
-        'base': 497
+        'base': 520
     },
-    
+
     {
         'id': 207,
         'name': "2-Year-Old",
-        'base': 332
+        'base': 350
     },
     {
         'id': 209,
         'name': "2-Year-Old",
-        'base': 332
+        'base': 350
     },
     {
         'id': 215,
@@ -66,8 +67,8 @@ orgs = [
     # }
     ]
 
-paymentOrgId = 206
-minTransactionId = 1 #28100
+paymentOrgId = 51
+minTransactionId = 28100
 retroactive = True
 now = datetime.now() - timedelta(days=9)
 
@@ -79,7 +80,7 @@ print "<a href=\"/RunScript/TuitionSummary/\">View Balances</a><br />"
 print "<a href=\"/RunScript/TuitionProgramTotals/\">Program Totals</a><br />"
 
 model.CurrentOrgId = paymentOrgId
-    
+
 # this is a container for collecting the "first child" of a given family, used to determine when a second child comes along.
 firstChildren = {}
 familyData = {}
@@ -97,59 +98,60 @@ monthToUseR = now
 monthName = monthToUse.strftime('%B')
 monthNameR = monthToUseR.strftime('%B')
 
+retroactive = retroactive and monthNameR != 'Aug'
 
 for cls in orgs:
     meetingCntSql = """SELECT COUNT(*) as mtgCnt FROM Meetings WHERE OrganizationId = {} AND YEAR(MeetingDate) = {} AND MONTH(MeetingDate) = {}""".format(cls['id'], monthToUse.year, monthToUse.month)
     meetingCnt = q.QuerySqlTop1(meetingCntSql).mtgCnt
-    
+
     if showTuitionAssessment:
         print "<h2>{}</h2>".format(cls['name'])
-        
+
         if cls.has_key('payPer') and cls['payPer'] == "meeting":
             print "<p><b>Tuition per Meeting - {} meetings in {}</b></p>".format(meetingCnt, monthName)
         else:
             print "<p><b>Tuition per Month</b></p>"
-    
+
     for student in q.QueryList("MemberTypeCodes( Org={} ) = 220[Member]".format(cls['id'])):
-        
+
         enrollmentDateSql = "SELECT EnrollmentDate FROM OrganizationMembers WHERE OrganizationId = {} AND PeopleId = {}".format(cls['id'], student.PeopleId)
         enrollmentDate = q.QuerySqlTop1(enrollmentDateSql).EnrollmentDate
-        
+
         lastChargeSql = """SELECT MAX(TransactionDate) as date
             FROM [Transaction] t
                 LEFT JOIN [TransactionPeople] tp ON t.OriginalId = tp.Id
                     WHERE t.OrgId = {} AND t.AdjustFee = 1 AND tp.PeopleId = {} AND Message LIKE '{}%'""".format(paymentOrgId, student.PeopleId, cls['name'])
         lastChargeDate = q.QuerySqlTop1(lastChargeSql)
         lastChargeDate = lastChargeDate if lastChargeDate is None else lastChargeDate.date
-        
+
         isFirstChild = True
         if firstChildren.has_key("{}".format(student.FamilyId)):
             isFirstChild = (firstChildren["{}".format(student.FamilyId)].PeopleId == student.PeopleId)
-            
+
         else:
             firstChildren["{}".format(student.FamilyId)] = student
-        
-        tuitionOverrideSql = """SELECT ome.IntValue as Tuition FROM OrgMemberExtra ome 
+
+        tuitionOverrideSql = """SELECT ome.IntValue as Tuition FROM OrgMemberExtra ome
         WHERE UPPER(ome.Field) = 'Tuition' AND ome.OrganizationId = {} AND ome.PeopleId = {}""".format(cls['id'], student.PeopleId)
-        
+
         tuitionOverride = q.QuerySqlTop1(tuitionOverrideSql)
-        
+
         meetingCntRSql = """SELECT COUNT(*) as mtgCnt FROM Meetings WHERE OrganizationId = {} AND YEAR(MeetingDate) = {} AND MONTH(MeetingDate) = {} AND MeetingDate > '{}'""".format(cls['id'], monthToUseR.year, monthToUseR.month, enrollmentDate)
         meetingCntR = q.QuerySqlTop1(meetingCntRSql).mtgCnt
-        
+
         tuitionLine = cls['name']
         depositLine = cls['name'] + " Deposit"
-        
+
         if tuitionOverride is None:
-            
+
             tuitionCharge = 1.0 * cls['base']
             depositCharge = 1.0 * cls['base']
             tuitionChargeR = 1.0 * cls['base'] * (meetingCntR > 0)
-            
+
             if cls.has_key('payPer') and cls['payPer'] == "meeting":
                 tuitionCharge *= meetingCnt
                 tuitionChargeR *= meetingCntR
-            
+
             if cls.has_key('discounts') and cls['discounts'] == False:
                 pass # no discounts
             else:
@@ -158,59 +160,59 @@ for cls in orgs:
                     tuitionChargeR = tuitionChargeR * (5.0/6.0)
                     tuitionLine = tuitionLine + " (Tenth Member Rate)"
                     # depositLine = depositLine + " (Tenth Member Rate)"  Do not apply discounts to Deposits.
-                
+
                 elif not isFirstChild:
                     tuitionCharge = tuitionCharge * (8.0/9.0)
                     tuitionChargeR = tuitionChargeR * (8.0/9.0)
                     tuitionLine = tuitionLine + " (Sibling Rate)"
                     # depositLine = depositLine + " (Sibling Rate)"  Do not apply discounts to Deposits.
-        
-        else: 
+
+        else:
             tuitionCharge = tuitionOverride.Tuition * 1.0
             tuitionChargeR = tuitionOverride.Tuition * 1.0
             depositCharge = tuitionOverride.Tuition * 1.0
             tuitionLine = tuitionLine + " (Special Rate)"
             depositLine = depositLine + " (Special Rate)"
-            
+
             if cls.has_key('payPer') and cls['payPer'] == "meeting":
                 tuitionCharge *= meetingCnt
                 tuitionChargeR *= meetingCntR
-        
+
         tuitionLineR = tuitionLine + " - " + monthNameR
         tuitionLine += " - " + monthName
-        
-        # Rounding, because the preschool wants that. 
+
+        # Rounding, because the preschool wants that.
         tuitionCharge = round(tuitionCharge, 0)
         tuitionChargeR = round(tuitionChargeR, 0)
         depositCharge = round(depositCharge, 0)
-        
+
         if enrollmentDate < lastChargeDate: # and not cls.has_key('payPer'):
             tuitionChargeR = 0.0
-        
+
         if showTuitionAssessment:
             # print "<p>{} {} ${:,.2f} {}</p>".format(student.PreferredName, student.LastName, depositCharge, depositLine)
             if tuitionChargeR > 0 and retroactive:
                 print "<p>{} {} ${:,.2f} {} (retro)</p>".format(student.PreferredName, student.LastName, tuitionChargeR, tuitionLineR)
             print "<p>{} {} ${:,.2f} {}</p>".format(student.PreferredName, student.LastName, tuitionCharge, tuitionLine)
-        
-        
+
+
         # Add to org, and add deposit if new to the org to account
         if not model.InOrg(student.PeopleId, paymentOrgId):
             model.JoinOrg(paymentOrgId, student.PeopleId)
-            
+
             # Add deposit if needed
             if monthToUse.month > 6:
                 model.AdjustFee(student.PeopleId, paymentOrgId, -depositCharge, depositLine)
-        
-        
+
+
         # TODO: Automatically add fee adjustments.
-        # TODO: Calculate proration of tuition. 
+        # TODO: Calculate prorating of tuition.
         if applyTuitionAssessment:
             if tuitionChargeR > 0 and tuitionLineR != tuitionLine:
                 model.AdjustFee(student.PeopleId, paymentOrgId, -tuitionChargeR, tuitionLineR)
             model.AdjustFee(student.PeopleId, paymentOrgId, -tuitionCharge, tuitionLine)
-        
-        
+
+
         # Initialize FamilyData dict for emails
         if not familyData.has_key("{}".format(student.FamilyId)):
             fd = {
@@ -226,133 +228,141 @@ for cls in orgs:
             }
         else:
             fd = familyData["{}".format(student.FamilyId)]
-        
+
         needsToAppend = True
         for part in fd['participants']:
             if part.PeopleId == student.PeopleId:
                 needsToAppend = False
                 break
-            
+
         if not needsToAppend:
             continue
-        
+
         fd['participants'].append(student)
         fd['participantPids'].append(str(student.PeopleId))
-        
+
         otherParent = model.ExtraValueInt(student.PeopleId, 'Parent')
         if otherParent != "":
             fd['otherParent'] = model.GetPerson(otherParent)
-        
-        
+
+
         transactionSql = """
-            SELECT 
+            SELECT
             FORMAT(t.TransactionDate, 'MMM dd, yyyy') as Date,
             FORMAT(t.Amt * -1, 'C') as Amount,
             t.Amt as AmtNum,
             t.Name,
             t.Description
-            
+
             FROM (
-            
+
             -- Deposit
             SELECT
                 t3.TransactionDate,
                 tp3.Amt * 1.0 as Amt,
                 COALESCE(REPLACE(t3.Message, 'APPROVED', 'Deposit'), 'Deposit') as Description,
-                p3.Name 
+                p3.Name
             FROM [TransactionPeople] tp3
                 LEFT JOIN People p3 ON tp3.PeopleId = p3.PeopleId
                 LEFT JOIN [Transaction] t3 ON tp3.Id = t3.Id
             WHERE tp3.Id IN (SELECT DISTINCT TranId FROM OrganizationMembers WHERE OrganizationId = {0} AND PeopleId IN ({1}))
-            
+
             UNION
-            
+
             -- Original Payment
             SELECT
                 t2.TransactionDate,
                 (tp2.Amt - t2.amtdue) * -1.0 as Amt,
                 COALESCE(REPLACE(t2.Message, 'APPROVED', 'Online Transaction'), 'Discount Code') as Description,
-                p2.Name 
+                p2.Name
             FROM [TransactionPeople] tp2
                 LEFT JOIN People p2 ON tp2.PeopleId = p2.PeopleId
                 LEFT JOIN [Transaction] t2 ON tp2.Id = t2.Id
             WHERE t2.coupon is null AND tp2.Id IN (SELECT DISTINCT TranId FROM OrganizationMembers WHERE OrganizationId = {0} AND PeopleId IN ({1}))
-            
+
             UNION
-            
+
             -- Original Payment
             SELECT
                 t4.TransactionDate,
                 t4.Amt * -1.0 as Amt,
                 COALESCE(REPLACE(t4.Message, 'APPROVED', 'Online Transaction'), 'Discount Code') as Description,
-                p4.Name 
+                p4.Name
             FROM [TransactionPeople] tp4
                 LEFT JOIN People p4 ON tp4.PeopleId = p4.PeopleId
                 LEFT JOIN [Transaction] t4 ON tp4.Id = t4.Id
             WHERE t4.coupon = 1 AND tp4.Id IN (SELECT DISTINCT TranId FROM OrganizationMembers WHERE OrganizationId = {0} AND PeopleId IN ({1}))
-            
+
             UNION
-                    
-            -- Adjustments 
-            SELECT 
+
+            -- Adjustments
+            SELECT
                 t1.TransactionDate,
                 t1.amt * -1.0 as Amt,
                 REPLACE(t1.Message, 'APPROVED', 'Online Transaction') as Description,
                 t1.Name
-            FROM [Transaction] t1 
+            FROM [Transaction] t1
             WHERE t1.Id <> t1.OriginalId AND t1.OriginalId IN (SELECT DISTINCT TranId FROM OrganizationMembers WHERE OrganizationId = {0} AND PeopleId IN ({1}))
-            
+
             ) as t
-            
+
             WHERE t.Amt <> 0
-            
+
             ORDER BY t.TransactionDate, t.Description, t.Name
         """.format(paymentOrgId, ", ".join(fd['participantPids']))
-        
+
         amtOwed = 0.0
-        
+
         table = "<table style=\"width:100%;\"><thead><tr><th>Date</th><th>Amount</th><th>Person</th><th>Description</th></tr></thead><tbody>"
         for row in q.QuerySql(transactionSql):
             table = table + "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>".format(row.Date, row.Amount, row.Name, row.Description)
             amtOwed += row.AmtNum or 0
         table += "</tbody></table>"
-        
+
         table += "<p><b>Balance: ${:,.2f}</b></p><br />".format(amtOwed)
-        
+
         fd['table'] = table
         amtOwed = amtOwed * 100
-        
+
         if amtOwed > 0:
             fd['hasBalanceDue'] = True
-            
+
         paylink = model.GetPayLink(student.PeopleId, paymentOrgId)
         if paylink is not None:
             paylink = paylink.replace(model.CmsHost, "")
             paylink = model.CmsHost + "/Logon?" + urllib.urlencode({"ReturnUrl": paylink})
             paylink = "<a href=\"{}\">paying online here</a>".format(paylink)
-        
+
         if paylink not in fd['paylinks']:
             fd['paylinks'].append(paylink)
-            
+
         fd['totalDue'] = amtOwed
-        
+
         familyData["{}".format(student.FamilyId)] = fd
-        
+
 emailData = []
 emailPids = []
-        
+
+if showTuitionAssessment:
+    print "<p><a href=\"?view=assessApply\" onclick=\"return confirm('Are you sure?  This cannot be undone.');\">Apply all of these additional values to family balances</a></p>"
+
+
+if showEmails:
+    print "<p><a href=\"?view=sendemails&to=all\" onclick=\"return confirm('Are you sure?  This cannot be undone.');\">Send Emails to ALL families with balance</a></p>"
+
+
 for fi in familyData:
     fam = familyData[fi]
-    
+
     p1 = fam['participants'][0].Family.HeadOfHousehold or None
     p2 = fam['participants'][0].Family.HeadOfHouseholdSpouse or None
     p3 = fam['otherParent']
-    
+
     if showEmails:
-        
+
         if not sendEmails:
             print "<h2>{} Family</h2>".format(familyData[fi]['participants'][0].LastName)
-        
+
         p1Name = None
         p2Name = None
         p3Name = None
@@ -366,18 +376,18 @@ for fi in familyData:
         if p3 is not None:
             p3Name = p3.Name
             pPids.append(p3.PeopleId)
-            
+
         pPids = map(str, pPids)
-        
+
         if not sendEmails:
             if familyData[fi]['hasBalanceDue']:
                 print "<p><b>Sends to: {} {} {}</b>  <a href=\"?view=sendemails&to={}\">Send Individually</a></p>".format(p1Name, p2Name, p3Name, ",".join(pPids))
             else:
                 print "<p><b>Does NOT Send to: {} {} {}</b>  <a href=\"?view=sendemails&to={}\">Send Individually</a></p>".format(p1Name, p2Name, p3Name, ",".join(pPids))
-        
+
         # if len(familyData[fi]['participants']) > 1:
         familyData[fi]['print'] = familyData[fi]['table']
-        
+
         amtOwed = familyData[fi]['totalDue']
         if amtOwed < 0:
             familyData[fi]['print'] += "<p>This credit of ${:,.2f} will be applied to future monthly billing cycles.  You do not need to do anything now.</p>".format(amtOwed * -0.01)
@@ -387,11 +397,11 @@ for fi in familyData:
             familyData[fi]['print'] += "<p><b>Please make a payment of ${:,.2f} by sending in a check, or {}.</b></p>".format(amtOwed * 0.01, " AND ".join(familyData[fi]['paylinks']))
         else:
             familyData[fi]['print'] += "<p>Since there is no outstanding balance, you don't need to do anything.</p>".format(student.PreferredName)
-        
-        
+
+
         if not sendEmails:
             print familyData[fi]['print']
-        
+
         if sendEmails and familyData[fi]['hasBalanceDue']:
             if p1 is not None:
                 emailPids.append(p1.PeopleId)
@@ -399,21 +409,21 @@ for fi in familyData:
                 recipientData.PeopleId = p1.PeopleId
                 recipientData.Summary = familyData[fi]['print']
                 emailData.append(recipientData)
-                
+
             if p2 is not None:
                 emailPids.append(p2.PeopleId)
                 recipientData = model.DynamicData()
                 recipientData.PeopleId = p2.PeopleId
                 recipientData.Summary = familyData[fi]['print']
                 emailData.append(recipientData)
-                
+
             if p3 is not None:
                 emailPids.append(p3.PeopleId)
                 recipientData = model.DynamicData()
                 recipientData.PeopleId = p3.PeopleId
                 recipientData.Summary = familyData[fi]['print']
                 emailData.append(recipientData)
-                
+
 
 if sendEmails:
     if Data.to == "all":
@@ -421,8 +431,24 @@ if sendEmails:
         emailPids = ",".join(emailPids)
     else:
         emailPids = Data.to
-        
+
     print "<p>Emails have been sent to:</p>"
-        
+
     print "PeopleIds = '{}'".format(emailPids)
     model.EmailContentWithPythonData("PeopleIds = '{}'".format(emailPids), 27796, "preschooltuition@tenth.org", "Tenth Preschool", "Preschool Tuition", emailData)
+
+    if Data.to == "all":
+        print """
+        <script>
+        alert("Emails sent");
+        window.location.href = "?";
+        </script>
+        """
+
+if applyTuitionAssessment:
+    print """
+    <script>
+    alert("Assessments Applied");
+    window.location.href = "?";
+    </script>
+    """
