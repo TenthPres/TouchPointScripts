6f2a838dd289544a13ba46fd11d987e7f7f58842
James K. <jkurtz@tenth.org>
2026-07-06 14:10:45 -0400

    Update SQL script to automatically reflect current active definition

--- ActiveRecordsOverTime.sql
+++ ActiveRecordsOverTime.sql
@@ -1,64 +1,51 @@
 -- Pckgd
 -- Title: Active Records Over Time
--- Description: Tracks the number of active records per year based on 2025 definition of active.
+-- Description: Tracks the number of active records per year based on CURRENT definition of active.
 -- Updates from: GitHub/TenthPres/TouchPointScripts/ActiveRecordsOverTime.sql
 -- Author: James at Tenth
 
+WITH FirstActivity AS (
+    SELECT MIN(ActivityDate) AS FirstDate
+    FROM (
+        SELECT MIN(ContributionDate) AS ActivityDate
+        FROM dbo.Contribution
 
-WITH FamiliesGiving AS (SELECT YEAR(cc.ContributionDate) AS ActivityYear, p.FamilyId
-                        FROM dbo.Contribution cc WITH (NOLOCK)
-                                 JOIN dbo.People p WITH (NOLOCK) ON p.PeopleId = cc.PeopleId
-                        WHERE cc.ContributionTypeId IN (1, 5, 8, 9, 10, 20)
-                          AND p.IsDeceased = 0
-                        GROUP BY YEAR(cc.ContributionDate), p.FamilyId
-                        HAVING COUNT(*) >= 1),
-     PeopleFromGiving AS (SELECT fg.ActivityYear, p.PeopleId
-                          FROM FamiliesGiving fg
-                                   JOIN dbo.People p WITH (NOLOCK) ON p.FamilyId = fg.FamilyId
-                          WHERE p.IsDeceased = 0),
-     Attenders AS (SELECT YEAR(a.MeetingDate) AS ActivityYear, a.PeopleId
-                   FROM dbo.Attend a WITH (NOLOCK)
-                            JOIN dbo.People p WITH (NOLOCK) ON p.PeopleId = a.PeopleId
-                   WHERE a.AttendanceFlag = 1
-                     AND p.IsDeceased = 0
-                   GROUP BY YEAR(a.MeetingDate), a.PeopleId
-                   HAVING COUNT(*) >= 2),
-     LegacyRegCount AS (SELECT YEAR(rd.Stamp) AS ActivityYear, rd.UserPeopleId AS PeopleId, COUNT(*) AS rCount
-                        FROM dbo.RegistrationData rd WITH (NOLOCK)
-                                 JOIN dbo.People p WITH (NOLOCK) ON rd.UserPeopleId = p.PeopleId
-                        WHERE rd.UserPeopleId IS NOT NULL
-                          AND rd.Completed = 1
-                          AND p.IsDeceased = 0
-                        GROUP BY YEAR(rd.Stamp), rd.UserPeopleId),
-     FormsRegCount AS (SELECT YEAR(rp.CompletedDate) AS ActivityYear, rp.PeopleId, COUNT(*) AS rCount
-                       FROM dbo.RegPeople rp WITH (NOLOCK)
-                                JOIN dbo.People p WITH (NOLOCK) ON rp.PeopleId = p.PeopleId
-                       WHERE rp.PeopleId IS NOT NULL
-                         AND rp.[Status] = 2
-                         AND p.IsDeceased = 0
-                       GROUP BY YEAR(rp.CompletedDate), rp.PeopleId),
-     RegistrationCount AS (SELECT ActivityYear, PeopleId, SUM(rCount) AS rCount
-                           FROM (SELECT ActivityYear, PeopleId, rCount
-                                 FROM LegacyRegCount
-                                 UNION ALL
-                                 SELECT ActivityYear, PeopleId, rCount
-                                 FROM FormsRegCount) r
-                           GROUP BY ActivityYear, PeopleId),
-     Registrants AS (SELECT ActivityYear, PeopleId
-                     FROM RegistrationCount
-                     WHERE rCount >= 2),
-     PPL AS (SELECT ActivityYear, PeopleId
-             FROM PeopleFromGiving
-             UNION ALL
-             SELECT ActivityYear, PeopleId
-             FROM Attenders
-             UNION ALL
-             SELECT ActivityYear, PeopleId
-             FROM Registrants),
-     DistinctPPL AS (SELECT ActivityYear, PeopleId
-                     FROM PPL
-                     GROUP BY ActivityYear, PeopleId)
-SELECT ActivityYear, COUNT(*) AS ActiveCount
-FROM DistinctPPL
-GROUP BY ActivityYear
-ORDER BY ActivityYear DESC
+        UNION ALL
+
+        SELECT MIN(MeetingDate)
+        FROM dbo.Attend
+
+        UNION ALL
+
+        SELECT MIN(Stamp)
+        FROM dbo.RegistrationData
+
+        UNION ALL
+
+        SELECT MIN(CompletedDate)
+        FROM dbo.RegPeople
+    ) x
+),
+Years AS (
+    SELECT YEAR(FirstDate) AS ActivityYear
+    FROM FirstActivity
+
+    UNION ALL
+
+    SELECT ActivityYear + 1
+    FROM Years
+    WHERE ActivityYear + 1 <= YEAR(GETDATE())
+)
+SELECT
+    y.ActivityYear,
+    DATEFROMPARTS(y.ActivityYear, 12, 31) AS AsOfDate,
+    COUNT(ar.PeopleId) AS ActiveCount
+FROM Years y
+CROSS APPLY dbo.ActiveRecords(
+    DATEFROMPARTS(y.ActivityYear, 12, 31)
+) ar
+GROUP BY
+    y.ActivityYear
+ORDER BY
+    y.ActivityYear DESC
+OPTION (MAXRECURSION 0);
